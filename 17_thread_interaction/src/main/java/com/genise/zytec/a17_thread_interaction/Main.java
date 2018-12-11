483d08e9c2fca3be26403cebfe77fca261de6044
Wisdozzh <18304094954@163.com>
2018-12-11 18:49:59 +0800

    rxjava

--- 17_thread_interaction/src/main/java/com/genise/zytec/a17_thread_interaction/Main.java
+++ 17_thread_interaction/src/main/java/com/genise/zytec/a17_thread_interaction/Main.java
@@ -1,11 +1,22 @@
 package com.genise.zytec.a17_thread_interaction;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 //        runThreadInteractionDemo();
 //        runWaitDemo();
-        runCustomizableThreadDemo();
+//        runCustomizableThreadDemo();
+//        List<String> provinces = Arrays.asList("北京市",
+//                "天津市",
+//                "上海市");
+        List<String> provinces = Arrays.asList();
+        for (String s:provinces) {
+            System.out.println(s);
+        }
+        System.out.println(provinces);
     }
 
     static void runThreadInteractionDemo() {
---
+++ 18_rxjava/src/main/java/com/genise/zytec/a18_rxjava/MainActivity.java
@@ -0,0 +1,63 @@
+package com.genise.zytec.a18_rxjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.genise.zytec.a18_rxjava.model.Repo;
+import com.genise.zytec.a18_rxjava.network.Api;
+
+import java.util.List;
+
+import io.reactivex.Scheduler;
+import io.reactivex.SingleObserver;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.text);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.github.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build();
+        Api api = retrofit.create(Api.class);
+
+        api.getRepos("rengwuxian")
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new SingleObserver<List<Repo>>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onSuccess(List<Repo> repos) {
+                        textView.setText(repos.get(0).getName());
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        String errorDesc = e.getMessage();
+                        if (errorDesc == null) {
+                            errorDesc = e.getClass().getName();
+                        }
+                        textView.setText(errorDesc);
+                    }
+                });
+    }
+}
